c993efd1cdead97314d98e8c087aecf9178e263d
ashok1558 <ashok1558@gmail.com>
2017-08-06 17:26:49 +0530

    Created a sample server

---
+++ myServer.js
@@ -0,0 +1,21 @@
+/*
+
+Created by Ashok Mishra
+Date 06-08-2017
+*/
+
+//importing required libraries
+var http = require("http");
+
+//Create HTTP server and listen on port 8000 for requests
+http.createServer(function(request, response){
+
+    // Set the response HTTP header with HTTP status and Content type
+    response.writeHead(200,{"Content-type":"text/plain"});
+
+    //setting response to any request
+    response.end("Hello World");
+}).listen(8000);
+
+//to notify that server is running on 127.0.0.1:8000
+console.log('Server running at http://127.0.0.1:8000/');
